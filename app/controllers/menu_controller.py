25f2dcf23fc087964dcd15a8d2544599ff86e745
Joziah Mays <joziah@MacBook-Pro.local>
2021-02-22 22:03:24 -0600

    added func for create_menu_items

--- app/controllers/menu_controller.py
+++ app/controllers/menu_controller.py
@@ -5,21 +5,17 @@ import json
 
 
 class MenuController:
-    #TODO : add current inventory amount
-    def get_all_menu_items(self):
+    def get_all_menu_items(self):  # TODO : add current inventory amount
         menu_list = (Stripe.Product.list(limit=30))
         print(menu_list)
         final_menu = {}
         for item in menu_list:
-            price_data = Stripe.Price.list(product=item["id"])
-            price = price_data["data"][0]["unit_amount"]/float(100)
+            price = self.get_product_price(item["id"])
             new_item = MenuItem(item["name"], item["description"], price, item["id"])
             final_menu[item["name"]] = json.dumps(new_item.__dict__)
         return final_menu
 
-
-#Unused for now but could be used later
-    def get_all_prices(self):
+    def get_all_prices(self):  # Unused for now but could be used later
         data_list = Stripe.Price.list()
         #print(data_list)
         price_list = {}
@@ -27,28 +23,62 @@ class MenuController:
             price_list[item["product"]] = item["unit_amount"]
         return price_list
 
+    def get_product_price(self, product_id):
+        price_data = Stripe.Price.list(product=product_id)
+        price = price_data["data"][0]["unit_amount"] / float(100)
+        return price
 
-    def get_menu_item(self, item_id):
-        pass
+    def get_menu_item_by_id(self, item_id):
+        try:
+            product = Stripe.Product.retrieve(item_id)
+            # print(product)
+            if product:
+                menu_item = {
+                    "name": product["name"],
+                    "description": product["description"],
+                    "price": self.get_product_price(item_id),
+                    "item_id": item_id,
+                    "type": product["metadata"]["Type"],
+                }
+                return menu_item
+        except Exception as e:
+            print('Something went wrong with: get_menu_item', e)
+            return None
 
     def create_menu_item(self, menu_item):
-        pass
+        try:
+            # registers a new product
+            new_product = Stripe.Product.create(
+                name=menu_item.item_name,
+                type="good",
+                description=menu_item.item_desc,
+                images=[menu_item.img],
+                metadata={
+                    "Type": menu_item.item_type
+                }
+            )
+
+            # registers a new price and binds it to the new product made above
+            Stripe.Price.create(
+                unit_amount=int(menu_item.item_price * 100),
+                currency="usd",
+                product=new_product["id"])
+
+            # registers a new SKU using the new product made above
+            Stripe.SKU.create(
+                price=int(menu_item.item_price * 100),
+                currency="usd",
+                product=new_product["id"],
+                inventory={"type": "finite", "quantity": menu_item.quantity},
+                image=menu_item.img)
+        except Exception as e:
+            #  print('Problem adding new menu item', e)
+            return None
+
+        return new_product["id"]
 
     def delete_menu_item(self, item_id):
         pass
 
     def update_menu_item(self, item_id):
         pass
-
-    def add_skus(self):
-        menu_list = (Stripe.Product.list(limit=30))
-        for item in menu_list:
-            price_data = Stripe.Price.list(product=item["id"])
-            price = price_data["data"][0]["unit_amount"]
-            currency = price_data["data"][0]["currency"]
-            Stripe.SKU.create(
-                price = price,
-                inventory={"type": "finite", "quantity": 50},
-                product=item["id"],
-                currency=currency,
-            )
--- app/routes/menu_routes.py
+++ app/routes/menu_routes.py
@@ -8,26 +8,41 @@ import json
 MenuController = MenuController()
 
 
-
 @app.route('/get_menu', methods=['GET'])
 def get_menu():
-    menu_list = MenuController.get_all_menu_items()
-    return menu_list
     """Endpoint that retrieves the menu for the client.
 
     Gets all the menu items from a restaurant and complies
     it together to form the menu. Essentially is get_all_items
     """
+    menu_list = MenuController.get_all_menu_items()
+    return menu_list
 
 
-@app.route('/menu_item/<int:item_id>', methods=['GET'])
+@app.route('/menu_item/<string:item_id>', methods=['GET'])
 def get_menu_item(item_id):
-    pass
+    data = MenuController.get_menu_item_by_id(item_id)
+    if data:
+        return jsonify(data), 200
+    else:
+        return jsonify({
+            "status": 400,
+            "message": f"Request could not be made. Check that an item with the id {item_id} actually exists."}), 400
 
 
 @app.route('/create_menu_item', methods=['POST'])
 def create_menu_item():
-    pass
+    data = json.loads(request.data)
+
+    menu_item = MenuItem(item_name=data["name"], item_desc=data["description"], item_price=data["price"],
+                         item_type=data["type"], quantity=data["quantity"], img=data["img"])
+
+    new_item_id = MenuController.create_menu_item(menu_item)
+
+    if new_item_id:
+        return jsonify({"status": "Success", "message": f'Item added with id: {new_item_id}'}), 200
+
+    return jsonify({"status": "Failed", "message": "Problem creating a new menu item."}), 500
 
 
 @app.route('/update_menu_item/<int:item_id>', methods=['PATCH'])
@@ -38,8 +53,3 @@ def update_menu_item(item_id):
 @app.route('/delete_menu_item/<int:item_id>', methods=['DELETE'])
 def delete_menu_item(item_id):
     pass
-
-@app.route('/add_SKUS', methods=['POST'])
-def add_skus():
-    MenuController.add_skus()
-    return "skus added"
