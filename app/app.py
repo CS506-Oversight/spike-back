e6b35ede3408a5f5dfd2d1e635d54e6107aac2a1
RaenonX <raenonx0710@gmail.com>
2021-02-23 23:21:18 -0600

    added missing routes registration
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@
 from flask import Flask
 from flask_cors import CORS
 
-from .routes import blueprint_menu, blueprint_order, blueprint_root
+from .routes import blueprint_menu, blueprint_order, blueprint_root, blueprint_user
 
 __all__ = ('create_app',)
 
@@ -14,6 +14,7 @@ def create_app():
 
     app.register_blueprint(blueprint_menu)
     app.register_blueprint(blueprint_order)
+    app.register_blueprint(blueprint_user)
     app.register_blueprint(blueprint_root)
 
     return app
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,3 +2,4 @@
 from .menu_routes import blueprint_menu
 from .order_routes import blueprint_order
 from .root import blueprint_root
+from .user_routes import blueprint_user
