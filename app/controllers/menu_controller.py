8d8a49fbcc2ded90bec18257c2ae8aecc024cb87
Joziah Mays <jmays2@wisc.edu>
2021-02-23 20:55:41 -0600

    added update functionality

--- app/controllers/menu_controller.py
+++ app/controllers/menu_controller.py
@@ -73,5 +73,70 @@ class MenuController:
         except Exception as e:
             return None
 
+    def get_product_price_id(self, item_id):
+        price_data = Stripe.Price.list(product=item_id)
+        price_id = price_data["data"][0]["id"]
+        print(price_id)
+        return price_id
+
+    def update_stripe_price(self, item_id, updated_price):
+        target_price = int(updated_price * 100)
+        price_list = Stripe.Price.list(product=item_id)
+        data = price_list['data']
+
+        price_exists = False
+
+        # check if there is already a matching price
+        for price_obj in data:
+            if price_obj['unit_amount'] == target_price:
+                price_exists = True
+                if not price_obj['active']:
+                    Stripe.Price.modify(price_obj['id'], active=True)
+            else:
+                if price_obj['active']:
+                    Stripe.Price.modify(price_obj['id'], active=False)
+
+        if not price_exists:
+            new_price = Stripe.Price.create(
+                unit_amount=int(updated_price * 100),
+                currency="usd",
+                product=item_id,
+                active=True)
+
+        return
+
+
     def update_menu_item(self, item_id, properties):
-        pass
+        """
+        Updates a menu item based in what the user wants to change. A user should only
+        be able to change the following properties if a given menu item:
+            - name
+            - desc
+            - price
+            - in_stock
+            - type
+            - img
+        """
+
+        doc_ref = db.collection('Menu')
+        doc = doc_ref.document(item_id)
+
+        doc.update(properties)
+
+        for prop in properties:
+            if prop == 'price':
+                price_id = self.get_product_price_id(item_id)
+                self.update_stripe_price(item_id, properties[prop])
+            if prop == 'description':
+                Stripe.Product.modify(item_id, description=properties[prop])
+            if prop == 'in_stock':
+                Stripe.Product.modify(item_id, active=properties[prop])
+            if prop == 'img':
+                images = [properties[prop]]
+                Stripe.Product.modify(item_id, images=images)
+            if prop == 'type':
+                Stripe.Product.modify(item_id, metadata={'Type': properties[prop]})
+            if prop == 'name':
+                Stripe.Product.modify(item_id, name=properties[prop])
+
+        return item_id
