4572071c5019b2b97714e9687f0c515b9a407f5b
tmehmen2 <tmehmen@wisc.edu>
2021-02-25 17:10:52 -0600

    reverted models to old python version

--- app/models/menu_model.py
+++ app/models/menu_model.py
@@ -8,11 +8,19 @@ __all__ = ('MenuItem',)
 @dataclass
 class MenuItem:
     """Data model for a menu item."""
+    def __init__(self, item_name, item_desc, item_price, in_stock, img, item_type, item_id=None):
+        this.item_name = item_name,
+        this.item_desc = item_desc
+        this.in_stock = in_stock
+        this.img = img
+        this.item_type = item_type
+        this.item_id = item_id
 
-    item_name: str
-    item_desc: str
-    item_price: float
-    in_stock: bool
-    img: str
-    item_type: str  # TODO: change to enum?
-    item_id: Optional[str] = field(default=None)
+
+    # item_name: str
+    # item_desc: str
+    # item_price: float
+    # in_stock: bool
+    # img: str
+    # item_type: str  # TODO: change to enum?
+    # item_id: Optional[str] = field(default=None)
--- app/models/user_model.py
+++ app/models/user_model.py
@@ -7,11 +7,19 @@ __all__ = ('User',)
 @dataclass
 class User:
     """User data model."""
+    def __init__(self, user_id, username, password, phone, address, email, type="staff"):
+        this.user_id = user_id
+        this.username = username
+        this.password = password
+        this.phone = phone
+        this.address = address
+        this.email = email
+        this.type = type
 
-    user_id: str
-    username: str
-    password: str
-    phone: str
-    address: str
-    email: str
-    type: str  # TODO: change to enum?
+    # user_id: str
+    # username: str
+    # password: str
+    # phone: str
+    # address: str
+    # email: str
+    # type: str  # TODO: change to enum?
